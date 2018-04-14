a0a1dd72ea67d26050ca8446833fe9ceda67a4a7
文艺小少年 <541190157@qq.com>
2018-04-14 19:34:57 +0800

    子设备号控制的LED驱动

--- first_drv.c
+++ first_drv.c
@@ -9,19 +9,67 @@
 #include <asm/arch/regs-gpio.h>
 #include <asm/hardware.h>
 
-static struct class *firstdrv_class;
-static struct class_device *firstdrv_class_dev;
+#define MINORBITS	20
+#define MINORMASK	((1U << MINORBITS) - 1)
 
+#define MINOR(dev)	((unsigned int) ((dev) & MINORMASK))
+
+
+static struct class *firstdrv_class;
+static struct class_device *firstdrv_class_dev[4];
+
+volatile unsigned long *gpfcon = NULL;
+volatile unsigned long *gpfdat = NULL;
 
 static int first_dev_open(struct inode *inode, struct file *file)
 {
-  printk("first_dev_open\n");
+
+  *gpfcon &= ~((0x3 << (4*2)) | (0x3 << (5*2)) | (0x3 << (6*2)));
+  *gpfcon |=  ((0x1 << (4*2))  | (0x1 << (5*2))  | (0x1 << (6*2)));
   return 0;
 }
 int major;
 static ssize_t first_dev_write(struct file *file, const char __user *buf, size_t count, loff_t * ppos)
 	{
-	  printk("first_dev_write\n");
+	  int minor = MINOR(file->f_dentry->d_inode->i_rdev);
+      int val;
+	  copy_from_user(&val,buf,count);
+   switch(minor)
+   	{
+   	  case 0 :
+   	  	{
+	  if (val == 1)
+	  	*gpfdat |= (1<<4) | (1<<5) | (1<<6);
+	  else 
+	  	*gpfdat &= ~((1<<4) | (1<<5) | (1<<6));
+   	  	}
+	  case 1 :
+	  	{
+	  if (val == 1)
+	  	*gpfdat |= (1<<4) | (1<<5) | (1<<6);
+	  else 
+	  	*gpfdat &= ~(1<<4) ;
+	  break;
+	  	}
+	  case 2 :
+	  	{
+	  if (val == 1)
+	  	*gpfdat |= (1<<4) | (1<<5) | (1<<6);
+	  else 
+	  	*gpfdat &= ~(1<<5) ;
+	  break;
+	  	}
+	  case 3 :
+	  	{
+	  if (val == 1)
+	  	*gpfdat |= (1<<4) | (1<<5) | (1<<6);
+	  else 
+	  	*gpfdat &= ~(1<<6) ;
+	  break;
+	  	}
+	  default :
+	    break;
+   	}  
 	  return 0;
 	}
 
@@ -35,21 +83,38 @@ static ssize_t first_dev_write(struct file *file, const char __user *buf, size_t
 
 int firse_drv_init(void)
 {
+  int minor = 0; 
+
   major = register_chrdev(0, "first_drv", &first_dev__fops);
 
-  firstdrv_class = class_create(THIS_MODULE, "firstdrv");
+  firstdrv_class = class_create(THIS_MODULE, "leds");
 
-  firstdrv_class_dev = class_device_create(firstdrv_class, NULL, MKDEV(major, 0), NULL, "xyz");
+  firstdrv_class_dev[0] = class_device_create(firstdrv_class, NULL, MKDEV(major, 0), NULL, "leds");
+
+ for (minor = 1; minor < 4; minor++)
+  	{
+  	  firstdrv_class_dev[minor] = class_device_create(firstdrv_class, NULL, MKDEV(major, minor), NULL, "led%d", minor);
+  	}
+
+  gpfcon = (volatile unsigned long*)ioremap(0x56000050,16);
+
+  gpfdat = gpfcon+1;
   
   return 0;
 }
 
 void firse_drv_exit(void)
 {
-  major = unregister_chrdev(0, "first_drv");
+  int minor;
+  major = unregister_chrdev(0, "leds");
 
-  class_device_unregister(firstdrv_class_dev);
+  class_device_unregister(firstdrv_class_dev[0]);
   class_destroy(firstdrv_class);
+  for (minor = 0; minor < 4; minor++)
+	{
+		class_device_unregister(firstdrv_class_dev[minor]);
+	}
+  iounmap(gpfcon);
 }
 
 module_init(firse_drv_init);
--- first_drv_test.c
+++ first_drv_test.c
@@ -9,9 +9,27 @@ int main(int argc,char **argv)
 
   int fd;
   int val = 1;
-  fd = open("/dev/xxx",O_RDWR);
-  if(fd<0)
-  	   printf("can't opem \n");
+  char* filename;
+  
+  filename = argv[1];
+  
+  fd = open(filename,O_RDWR);
+  if (fd < 0)
+    {
+        printf("error, can't open %s\n", filename);
+        return 0;
+    }
+  if (argc !=3)
+  	{
+  	  printf("Usage : \n");
+	  printf("%s %d <on|off>\n",argv[0],argv[1]);
+	  return 0;
+  	}
+  if (strcmp(argv[2],"on") == 0)
+  	val = 0;
+  else 
+  	val = 1;
+  
   write(fd,&val,4);
   return 0;
 }
