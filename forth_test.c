258692e677619db3efe359f1f1d3d009b0dd81d1
文艺小少年 <541190157@qq.com>
2018-04-20 12:31:44 +0800

    poll机制

---
+++ forth_test.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <poll.h>
+
+
+int main(int argc,char **argv)
+{
+
+  int fd;
+  int cnt = 0;
+  unsigned char key_val;
+  int ret ;
+
+  
+  struct pollfd fds[1];   //只查询一个程序
+  fd = open("/dev/buttons",O_RDWR);
+  if (fd < 0)
+    {
+        printf("error, can't open \n");
+    }
+  fds[0].fd = fd;
+  fds[0].events = POLLIN;
+while(1)
+  	{
+  	  ret = poll(fds, 1 , 5000);
+	  if(ret == 0)
+	  	{
+	  	  printf("tine out\n");
+	  	}
+	  else 
+	  	{
+  	  read(fd, &key_val, 1);
+	  printf("key_val = 0x%x \n",key_val);
+	  	}
+  	}
+  return 0;
+
+}
+
+
+   
+
+
